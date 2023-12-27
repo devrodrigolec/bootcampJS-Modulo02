55a6e8dd927bc7571372d9a62f9145c6d41327f4
Rodrigo <devrodrigolec@gmail.com>
2023-12-27 18:28:11 +0100

    laboratorio modulo 02

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,3 @@
-import "./style.css";
+/* Se restan los 18 € del total de la cena (120 €) y luego se divide entre el total de 6 personas. El resultado es 17 € por persona. */
 
-console.log("Hello Typescript!");
+console.log((120 - 18) / 6);
